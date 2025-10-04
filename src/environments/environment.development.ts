b88dbe83a7a7495e175c829c98cd803b4ce4d5e7
Mikołaj Józefiak <m.jozefiak02@gmail.com>
2025-10-04 21:51:22 +0200

    env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  geminiApiKey: 'AIzaSyD1Ahh4a5VdjqtqYO8YPMRVAf4gfHhpdXs', // Development Gemini API key
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  geminiApiKey: '', // Production Gemini API key
+};
