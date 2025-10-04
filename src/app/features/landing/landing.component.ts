1cee94ee463934f1f91831a0376887acf083c07a
Mikołaj Józefiak <m.jozefiak02@gmail.com>
2025-10-05 00:01:00 +0200

    landing

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,12 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatComponent } from '../../pattern/chat/chat.component';
+import { CardComponent, InsuranceCardData } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ChatComponent],
+  imports: [CardComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingComponent {}
+export class LandingComponent {
+  insuranceCards: InsuranceCardData[] = [
+    {
+      title: 'Buy insurance',
+      price: 'from 130 zł',
+      people: '2 people',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      buttonText: 'Learn more',
+    },
+    {
+      title: 'Buy insurance',
+      price: 'from 130 zł',
+      people: '2 people',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      buttonText: 'Learn more',
+    },
+    {
+      title: 'Buy insurance',
+      price: 'from 230 zł',
+      people: '2 people',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+      buttonText: 'Learn more',
+    },
+  ];
+
+  onCardButtonClick(): void {
+    console.log('Card button clicked');
+  }
+}
